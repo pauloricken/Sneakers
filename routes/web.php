dd2a7d210830016f58ad1fc7453490b75a8c5ead
paulo <paulo.antunes@unidavi.edu.br>
2022-11-07 16:20:40 -0300

    ATT
    ATT

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('site.home');
+
+}) ->name('site.home');
+
+
+
+
+Route::get('/cursos', function () {
+    return view('welcome');
+}) ->name('site.cousers');
+
+
+
+Route::get('/contatos', function () {
+    return view('welcome');
+
+}) ->name('site.contact');
+
